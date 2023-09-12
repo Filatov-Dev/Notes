0746521c8a7a3a874c83a0822e47416f52591e65
Filatov_Yurii <filatov.ios.dev@gmail.com>
2023-09-12 13:36:29 +0300

    Поправил формирование ячейки

--- Notes/Model/NoteCell.swift
+++ Notes/Model/NoteCell.swift
@@ -9,52 +9,76 @@ import UIKit
 
 class NoteCell: UITableViewCell {
     
-    var noteImageView  = UIImageView()
-    var noteTitleLabel = UILabel()
+    static let identifier = "NoteCell"
+    
+    private let noteImageView: UIImageView  = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(systemName: "questionmark")
+        iv.tintColor = .label
+        
+        return iv
+    }()
+    
+    private let noteTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        label.text = "Error"
+        
+        return label
+    }()
+    private let noteDescription: UITextView = {
+        let description = UITextView()
+        description.text = "Error"
+        description.textAlignment = .left
+        description.font = .systemFont(ofSize: 24, weight: .medium)
+        
+        return description
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addSubview(noteImageView)
-        addSubview(noteTitleLabel)
-        
-        configureImageView()
-        configureTitleLabel()
-        setImageConstraints()
-        setTitleConstraints()
+        self.setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func set(note: NoteModel) {
-        noteImageView  = note.image
-        noteTitleLabel = note.title
+    
+    private func setupUI() {
+        self.contentView.addSubview(noteImageView)
+        self.contentView.addSubview(noteTitleLabel)
+        self.contentView.addSubview(noteDescription)
+     
+        setConstraints()
     }
     
-    func configureImageView() {
-        noteImageView.layer.cornerRadius = 10
-        noteImageView.clipsToBounds      = true
+    public func setData(note: NoteModel) {
+        noteImageView.image  = note.image
+        noteTitleLabel.text = note.title
+        noteDescription.text = note.description
     }
     
-    func configureTitleLabel() {
-        noteTitleLabel.numberOfLines             = 0
-        noteTitleLabel.adjustsFontSizeToFitWidth = true
-    }
-    
-    func setImageConstraints() {
-        noteImageView.translatesAutoresizingMaskIntoConstraints                                                = false
-        noteImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive                                = true
-        noteImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive                  = true
-        noteImageView.heightAnchor.constraint(equalToConstant: 30).isActive                                    = true
-        noteImageView.widthAnchor.constraint(equalTo: noteImageView.heightAnchor, multiplier: 16/9).isActive   = true
-    }
-    
-    func setTitleConstraints() {
-        noteTitleLabel.translatesAutoresizingMaskIntoConstraints                                               = false
-        noteTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive                               = true
-        noteTitleLabel.leadingAnchor.constraint(equalTo: noteImageView.trailingAnchor, constant: 20).isActive  = true
-        noteTitleLabel.heightAnchor.constraint(equalToConstant: 30).isActive                                   = true
-        noteTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive              = true
+    private func setConstraints() {
+        noteImageView.translatesAutoresizingMaskIntoConstraints = false
+        noteTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        noteDescription.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            noteImageView.topAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.topAnchor),
+            noteImageView.bottomAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.bottomAnchor),
+            noteImageView.leadingAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.leadingAnchor),
+            noteImageView.widthAnchor.constraint(equalToConstant: 85),
+            noteImageView.heightAnchor.constraint(equalToConstant: 85),
+            
+            noteTitleLabel.leadingAnchor.constraint(equalTo: self.noteImageView.trailingAnchor, constant: 16),
+            noteTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -12),
+            noteTitleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            noteTitleLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+        ])
+        
     }
 }
